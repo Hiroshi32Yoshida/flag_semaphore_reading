129cdba886fe6c36297dcba2a50c3c485e8cc611
Hiroshi <64144258+Hiroshi32Yoshida@users.noreply.github.com>
2021-01-15 23:59:34 +0900

    Update sketch.js

--- deploying/sketch.js
+++ deploying/sketch.js
@@ -45,21 +45,13 @@ function brainLoaded() {
 
 function classifyPose() {
   if (pose) {
-    //let inputs = [];
-    //for (let i = 0; i < pose.keypoints.length; i++) {
-    //  let x = pose.keypoints[i].position.x;
-    //  let y = pose.keypoints[i].position.y;
-    //  inputs.push(x);
-    //  inputs.push(y);
-    //}
-    //brain.classify(inputs, gotResult);
-    let inputs = pose_normalize(pose.keypoints);
-    if(inputs == null){
-      poseLabel = '';
-      setTimeout(classifyPose, 100);
-    }else{
-      brain.classify(inputs, gotResult);
-    }
+//    let inputs = pose_normalize(pose.keypoints);
+//    if(inputs == null){
+//      poseLabel = '';
+//      setTimeout(classifyPose, 100);
+//    }else{
+//      brain.classify(inputs, gotResult);
+//    }
   } else {
     setTimeout(classifyPose, 100);
   }
